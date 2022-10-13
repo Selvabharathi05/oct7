2d2c9048320c74f311412f836a01193dcdfc4ba5
Selvabharathi <bharathikavap@gmail.com>
2022-10-13 07:45:14 +0530

    Add project files.

---
+++ Book/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+    public class Books
+    {
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+
+        public string Author { get; set; }
+        public double Price { get; set; }
+
+        public string Category { get; set; }
+    }
+}
